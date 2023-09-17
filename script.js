6ce1554792ba19437c24b09eb3b8d3c93451bc5e
amirkhan14771 <amirkhan14771@example.com>
2023-09-17 09:48:18 +0530

    new file uploaded

---
+++ script.js
@@ -0,0 +1,26 @@
+var myName = "Amir khan"; //data type string
+var myNumber = 7357101348; //data type number
+var myAcNo = 918010000290600; //data type number
+var myBnkName = "Uco Bank"; //data type string
+var myAdd = "Joshi Marg jhotwara, jaipur"; //data type string
+var Car = false; //data type boolean
+var Bike = true; //data type boolean
+var money; //data type undifined
+
+let myName = "Amir khan"; //data type string
+let myNumber = 7357101348; //data type number
+let myAcNo = 918010000290600; //data type number
+let myBnkName = "Uco Bank"; //data type string
+let myAdd = "Joshi Marg jhotwara, jaipur"; //data type string
+let Car = false; //data type boolean
+let Bike = true; //data type boolean
+let money; //data type undifined
+
+const myName = "Amir khan"; //data type string
+const myNumber = 7357101348; //data type number
+const myAcNo = 918010000290600; //data type number
+const myBnkName = "Uco Bank"; //data type string
+const myAdd = "Joshi Marg jhotwara, jaipur"; //data type string
+const Car = false; //data type boolean
+const Bike = true; //data type boolean
+// const money; //data type undifined
