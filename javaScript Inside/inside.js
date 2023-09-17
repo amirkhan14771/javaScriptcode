a81b0187cd58429249e8879ed1d8349cf2dcc250
amirkhan14771 <amirkhan14771@example.com>
2023-09-17 09:46:24 +0530

    new folder uploaded

---
+++ javaScript Inside/inside.js
@@ -0,0 +1,35 @@
+// let i = 10;
+// const j = 40;
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+// function one() {
+//   let i = 60;
+//   let j = 100;
+//   console.log(i, j);
+//   console.log(1);
+// }
+// setInterval(() => {
+//   console.log("Hello");
+// }, 0);
+// function two() {
+//   console.log(2);
+// }
+// two();
+
+// Promise.resolve(100).then((value) => {
+//   console.log(value);
+// });
+
+// setTimeout(() => {
+//   console.log("Amir");
+// }, 2000);
+
+// setInterval(() => {
+//   console.log("Khan");
+// }, 1000);
+
+// Promise.resolve(900).then((value) => {
+//   console.log(value);
+// });
